e5ae801e898bf07ff04b2db52e88e07f1a5c7d29
dbarber96b <87770160+dbarber96b@users.noreply.github.com>
2022-07-15 08:24:44 -0500

    Update Drifter.js

--- Drifter.js
+++ Drifter.js
@@ -30,40 +30,39 @@ AddSubClass("warlock", "the drifter", {
 	source : ["GMBDB"],
 	spellcastingExtra : ["bane", "bless", "enhance ability", "pass without trace", "blink", "nondetection", "death ward", "freedom of movement", "circle of power", "passwall"],
 	features : {
-		"subclassfeature1" : {
-			name : "Road Less Taken",
-			source : ["GMBDB"],
-			minlevel : 1,
-			description : "\n   " + "As an action, I can cast the augury spell" + "\n   " + "I must be at a crossroads, literally or figuratively to do this",
-			recovery : "short rest",
-			usages : 1,
-			action : ["action", ""]
-		},
-    "subclassfeature1.1" : {
-			name : "Drifter's Gift",
-			source : ["GMBDB"],
+		    "subclassfeature1" : {
+			name : "Hex Warrior",
+			source : ["X", 55],
 			minlevel : 1,
 			description : desc([
-				"I gain proficiency with medium armor, as well as longswords, rapiers, and scimitars.",
+				"I gain proficiency with medium armor and longswords, rapiers, and scimitars",
 				"When I finish a long rest, I can imbue one weapon I touch with my will",
 				"Until my next long rest, I can use it with Charisma instead of Strength or Dexterity",
 				"I have to be proficient with the weapon and it can't have the two-handed property",
-				"This benefit also works with every weapon from Pact of the Blade, with no restriction",
+				"This benefit also works with every weapon from Pact of the Blade, with no restriction"
 			]),
 			armorProfs : [false, true, false, false],
-			weaponProfs : [false, false, ["longsword", "rapier", "scimitar"]],
+			weaponProfs : [false, false, ["longsword, rapier, scimitar"]],
 			calcChanges : {
 				atkAdd : [
 					function (fields, v) {
-						var hasPactWeapon = GetFeatureChoice('class', 'warlock', 'pact boon') == 'pact of the blade';
-						if (What('Cha Mod') > What(AbilityScores.abbreviations[fields.Mod - 1] + ' Mod') && (v.pactWeapon || v.theWea.pactWeapon || (hasPactWeapon && (/\bpact\b/i).test(v.WeaponText)) || (/^(?=.*hexblade)(?!.*((^|[^+-]\b)2|\btwo).?hand(ed)?s?\b).*$/i).test(v.WeaponText))) {
+						if (What('Cha Mod') > What(AbilityScores.abbreviations[fields.Mod - 1] + ' Mod') && (v.pactWeapon || v.theWea.pactWeapon || /^(?=.*drifter)(?!.*((^|[^+-]\b)2|\btwo).?hand(ed)?s?\b).*$/i.test(v.WeaponText))) {
 							fields.Mod = 6;
 						};
 					},
-					"If I include the word 'Drifter' in the name of a weapon that is not two-handed, it gets treated as the weapon I imbued to use Charisma instead of Strength or Dexterity, if my Charisma modifier is higher than the ability it would otherwise use. Alternatively, if I have the Pact of the Blade feature, this will also work if I include 'Pact' in the name of a weapon, regardless if it has the two-handed property."
+					"If I include the word 'Drifter' in the name of a weapon that is not two-handed, it gets treated as the weapon I imbued to use Charisma instead of Strength or Dexterity, if my Charisma modifier is higher than the ability it would otherwise use. Alternatively, if I have the Pact of the Blade feature, this will also work for any weapons set to be my Pact Weapon."
 				]
 			}
 		},
+		"subclassfeature3.1" : {
+				name : "One Step Ahead",
+				source : ["GMBDB"],
+				minlevel : 3,
+				description : desc([
+					"I add my Charisma modifier to initiative rolls"
+				]),
+				addMod : { type : "skill", field : "Init", mod : "max(Cha|0)", text : "I can add my Charisma modifier to initiative rolls." }
+			},
 		"subclassfeature6" : {
 			name : "Everything's Looking Up",
 			source : ["GMBDB"],
@@ -71,10 +70,15 @@ AddSubClass("warlock", "the drifter", {
 			description : "\n   " + "I have advantage on all Charisma (Persuasion), Charisma (Deception), and Charisma (Performance) checks against any creature of my choice that isn’t hostile toward you " + "\n   " + "I have advantage on Dexterity saving throws against effects such as traps and spells. To gain this benefit, I can't be incapacitated",
 		},
 		"subclassfeature10" : {
-			name : "One Step Ahead",
-			source : ["P", 109],
+			name : "Superior Mobility",
+			source : ["X", 47],
 			minlevel : 10,
-			description : "\n   " + "I now add my Charisma modifier to initiative rolls and Dexterity saving throws",
+			description : "\n   " + "I gain +10 ft to my walking speed (and swimming/climbing speed, if applicable)",
+			speed : {
+				walk : { spd : "+10", enc : "+10" },
+				climb : { spd : "_10", enc : "_10" },
+				swim : { spd : "_10", enc : "_10" }
+			}
 		},
 		"subclassfeature14" : {
 			name : "Closing the Distance",
