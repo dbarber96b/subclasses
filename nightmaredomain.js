0d4709a22cd51dcde2ba18458d731d3f5a650e16
dbarber96b <87770160+dbarber96b@users.noreply.github.com>
2022-12-01 09:45:01 -0600

    Update nightmaredomain.js

--- nightmaredomain.js
+++ nightmaredomain.js
@@ -8,65 +8,70 @@
 			name : "Bonus Cantrip",
 			source : ["GMBDB"],
 			minlevel : 1,
-			description : "\n   " + "I learn the Vicious Mockery cantrip",
+			description : "\n   " + "I learn the Vicious Mockery cantrip if I didn't already know it",
 			spellcastingBonus : {
-				name : "Nightmare Domain Cantrip",
+				name : "Bonus Cantrip (Vicious Mockery)",
 				spells : ["vicious mockery"],
-				times : 2
+				selection : ["vicious mockery"]
 			}
 		},
+		"subclassfeature1.1" : {
+			name : "Cry In the Night",
+			source : ["GMBDB"],
+			minlevel : 1,
+			description : "\n   " + "As a reaction, when a creature I can see within 30 ft hits me, I can rebuke with their worst fears" + "\n   " + "It takes 2d8 psychic damage that a Wis save can halve",
+			usages : "Wisdom modifier per ",
+			usagescalc : "event.value = Math.max(1, What('Wis Mod'));",
+			recovery : "long rest",
+			action : ["reaction", ""]
+		},
 		"subclassfeature2" : {
-			name : "Channel Divinity: Traversal",
+			name : "Channel Divinity: Free Falling",
 			source : ["GMBDB"],
 			minlevel : 2,
 			description : desc([
-				"I can take the Dash action asa bonus action",
-				"Climbing doesn't cost me any extra movement",
-				"I add my Wisdom modifier to Strength (Athletics), Dexterity (Acrobatics, Stealth), and the distances you can cover making a long or high jump (minimum +1)"
+				"I can cause all creatures I see within 10 ft to see the terrain fall away from under their feet.",
+				"The creatures must make a Wisdom saving throw, falling prone and taking 3d6 psychic damage on a fail",
+				"On a save, they take half damage, and are not knocked prone."
 			])
 		},
 		"subclassfeature6" : {
-			name : "Channel Divinity: Spellsteal",
+			name : "Channel Divinity: Loathsome Knell",
 			source : ["GMBDB"],
 			minlevel : 6,
 			description : desc([
-				"When I cast Counterspell, I can can expend a use of my Channel Divinity to gain a bonus to the required spellcasting ability check equal to half my cleric level",
-				"If is at least 1st level, and of a level I can cast, I steal knowledge of that spell.",
-				"I can cast this spell a number of times equal to my Wisdom modifier until a long rest."
+				"When I deal psychic damage, I can also project a phantasmal bell into the target’s mind and ring it",
+				"This ringing inflicts short term madness lasting 1d10 minutes (see Madness rules)."
 			])
 		},
 		"subclassfeature8" : {
 			name : "Divine Strike",
-			source : ["X", 19],
+			source : ["GMBDB"],
 			minlevel : 8,
 			description : "\n   " + "Once per turn, when I hit a creature with a weapon attack, I can do extra damage",
 			additional : levels.map(function (n) {
 				if (n < 8) return "";
-				return "+" + (n < 14 ? 1 : 2) + "d8 force damage";
+				return "+" + (n < 14 ? 1 : 2) + "d8 psychic damage";
 			}),
 			calcChanges : {
 				atkAdd : [
 					function (fields, v) {
 						if (classes.known.cleric && classes.known.cleric.level > 7 && !v.isSpell) {
-							fields.Description += (fields.Description ? '; ' : '') + 'Once per turn +' + (classes.known.cleric.level < 14 ? 1 : 2) + 'd8 force damage';
+							fields.Description += (fields.Description ? '; ' : '') + 'Once per turn +' + (classes.known.cleric.level < 14 ? 1 : 2) + 'd8 psychic damage';
 						}
 					},
-					"Once per turn, I can have one of my weapon attacks that hit do extra force damage."
+					"Once per turn, I can have one of my weapon attacks that hit do extra psychic damage."
 				]
 			}
 		},
 		"subclassfeature17" : {
-			name : "Transcendence",
+			name : "The Mind of Madness",
 			source : ["GMBDB"],
 			minlevel : 17,
 			description : desc([
-				"The Gate and Astral Projection spells are cleric spells for me, and are always prepared, and do not count against the muber of spells I can prepare.",
-				"Freedom of Movement and Dimension Door can now have a casting time of a bonus action instead of and action"
-			]),
-			spellcastingBonus : {
-			name : "Transcendence",
-			spells : ["gate", "astral projection"],
-			selection : ["gate", "astral projection"],
-			firstCol : "1" } }
+				"My Loathsome Knell instead inflicts long-term madness lasting 1d10x10 hours and an indefinite madness flaw (see Madness rules in DM rulebook).",
+				"This flaw can only be removed by greater restoration or 30 days of rest."
+			])
+		}
 	}
 });
