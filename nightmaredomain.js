f011293138ddd2ed57784a05bc9da15273844ada
dbarber96b <87770160+dbarber96b@users.noreply.github.com>
2022-12-01 08:49:45 -0600

    Create nightmaredomain.js

---
+++ nightmaredomain.js
@@ -0,0 +1,72 @@
+ AddSubClass("cleric", "nightmare domain", {
+	regExpSearch : /^(?=.*(cleric|priest|clergy|acolyte))(?=.*(night|nightmare|dreamer)).*$/i,
+	subname : "Nightmare Domain",
+	source : ["GMBDB"],
+	spellcastingExtra : ["tasha's hideos laughter", "illusory script", "phantasmal force", "silence", "major image", "slow", "hallucinatory terrain", "phantasmal killer", "dream", "geas"],
+	features : {
+		"subclassfeature1" : {
+			name : "Bonus Cantrip",
+			source : ["GMBDB"],
+			minlevel : 1,
+			description : "\n   " + "I learn the Vicious Mockery cantrip",
+			spellcastingBonus : {
+				name : "Nightmare Domain Cantrip",
+				spells : ["vicious mockery"],
+				times : 2
+			}
+		},
+		"subclassfeature2" : {
+			name : "Channel Divinity: Traversal",
+			source : ["GMBDB"],
+			minlevel : 2,
+			description : desc([
+				"I can take the Dash action asa bonus action",
+				"Climbing doesn't cost me any extra movement",
+				"I add my Wisdom modifier to Strength (Athletics), Dexterity (Acrobatics, Stealth), and the distances you can cover making a long or high jump (minimum +1)"
+			])
+		},
+		"subclassfeature6" : {
+			name : "Channel Divinity: Spellsteal",
+			source : ["GMBDB"],
+			minlevel : 6,
+			description : desc([
+				"When I cast Counterspell, I can can expend a use of my Channel Divinity to gain a bonus to the required spellcasting ability check equal to half my cleric level",
+				"If is at least 1st level, and of a level I can cast, I steal knowledge of that spell.",
+				"I can cast this spell a number of times equal to my Wisdom modifier until a long rest."
+			])
+		},
+		"subclassfeature8" : {
+			name : "Divine Strike",
+			source : ["X", 19],
+			minlevel : 8,
+			description : "\n   " + "Once per turn, when I hit a creature with a weapon attack, I can do extra damage",
+			additional : levels.map(function (n) {
+				if (n < 8) return "";
+				return "+" + (n < 14 ? 1 : 2) + "d8 force damage";
+			}),
+			calcChanges : {
+				atkAdd : [
+					function (fields, v) {
+						if (classes.known.cleric && classes.known.cleric.level > 7 && !v.isSpell) {
+							fields.Description += (fields.Description ? '; ' : '') + 'Once per turn +' + (classes.known.cleric.level < 14 ? 1 : 2) + 'd8 force damage';
+						}
+					},
+					"Once per turn, I can have one of my weapon attacks that hit do extra force damage."
+				]
+			}
+		},
+		"subclassfeature17" : {
+			name : "Transcendence",
+			source : ["GMBDB"],
+			minlevel : 17,
+			description : desc([
+				"The Gate and Astral Projection spells are cleric spells for me, and are always prepared, and do not count against the muber of spells I can prepare.",
+				"Freedom of Movement and Dimension Door can now have a casting time of a bonus action instead of and action"
+			]),
+			spellcastingBonus : {
+			name : "Transcendence",
+			spells : ["gate", "astral projection"],
+			selection : ["gate", "astral projection"],
+			firstCol : "1" } }
+	}
+});
