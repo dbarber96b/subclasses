0b53d1083229ae50209349e6758cbd62790cd00f
dbarber96b <87770160+dbarber96b@users.noreply.github.com>
2023-10-28 12:07:39 -0500

    Update IsendranLegends.js

--- IsendranLegends.js
+++ IsendranLegends.js
@@ -748,6 +748,154 @@ RaceList["fey'ri"] = {
 		}
 	}
 });
+AddSubClass("fighter", "duelist", {
+	regExpSearch : /^(?=.*duelist)(?=.*swordsman).*$/i,
+	subname : "Duelist",
+	source : ["LoI"],
+	fullname : "Duelist",
+	abilitySave : 2,
+	ablitySaveAlt : 5,
+	features : {
+		"subclassfeature3" : {
+			name : "Blinkstep",
+			source : ["LoI"],
+			minlevel : 3,
+			usages : [0, 0, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, "Unlimited", "Unlimited"],
+			description : "\n    " + "As an action, I can move in a line up to my movement speed." +
+			"\n    " + "However, I can only do this if I have a one handed sword weapon." +
+			"\n    " + "I make an attack action on all enemy creatures in that line." + 
+			"\n    " + "The number of attack increases with each extra attack feature in the fight class.",
+			action : [["action", ""]],
+			recovery : "short rest"
+		},
+		"subclassfeature3.1" : {
+			name : "Weapon Connection",
+			source : ["LoI"],
+			minlevel : 3,
+			description : "\n    " + "As an bonus action, I can summon my swordsman weapon"
+			+ "\n    " + "back to me as long as it is on the same plane of existence as me.",
+			action : [["bonus action", ""]]
+		},
+		"subclassfeature7" : {
+			name : "Stances",
+			source : ["LoI"],
+			minlevel : 7,
+			description : "\n    " + "I can now activate stances, each having an indepedent action type.",
+			"The Acrobat" : {
+				name : "The Acrobat",
+				source : ["LoI"],
+				minlevel : 7,
+				description : "\n   " + "Stay light on your free as you face down your enemy." +
+				"\n    " + "As a bonus action, you have advantage on all Dexterity ability rolls and saving throws" +
+				"\n    " + "but disadvantage on all Strength ability rolls and saving throws while this is active.",
+				action : [["bonus action", ""]]
+			},
+			"The Berserker" : {
+				name : "The Berserker",
+				source : ["LoI"],
+				minlevel : 7,
+				description : "\n    " + "All that matters is the blood being spilt." +
+				"\n    " + "It doesn't matter if its you or your opponent, someone must die!" +
+				"\n    " + "When this is activated as a bonus action, you have advantage on your attack rolls" +
+				"\n    " + "However, all attack rolls on you have advantage as well.",
+				action : [["bonus action", ""]]
+			},
+			"The Brute" : {
+				name : "The Brute",
+				source : ["LoI"],
+				minlevel : 7,
+				description : "\n    " + "Use your strength to your advantage." +
+				"\n    " + "As a bonus action, you have advantage on any strength based ability roll or saving throw" +
+				"\n    " + "but have disadvantage on any dexterity based ability roll or saving throw.",
+				action : [["bonus action", ""]]
+			},
+			"The Dancer" : {
+				name : "The Dancer",
+				source : ["LoI"],
+				minlevel : 7,
+				description : "\n    " + "As a reaction, you can roll acrobatics to dodge an opponent's attack," +
+				"\n    " + "moving 5 feet to their left or right should you match or beat their attack roll.",
+				action : [["reaction action", ""]]
+			},
+			"The Defender" : {
+				name : "The Defender",
+				source : ["LoI"],
+				minlevel : 7,
+				description : "\n    " + "As a reaction, you can ready your weapon to defend yourself," +
+				"\n    " + "adding your proficiency bonus to your AC when an enemy targets you with a weapon attack.",
+				action : [["reaction", ""]]
+			},
+			"The Shield" : {
+				name : "The Shield",
+				source : ["LoI"],
+				minlevel : 7,
+				description : "\n    " + "As a reaction, you can impose disadvantage on a " +
+				"\n    " + "creature that makes a melee attack on an ally within 5 feet of you." +
+				"\n    " + "However, all ranged attacks have advantage on you since you focused on melee defense.",
+				action : [["reaction", ""]]
+			},
+			toNotesPage : [{
+				name : "Stances",
+				note : "\n" + "\u2022 The Acrobat" + 
+				"\n    " + "Stay light on your free as you face down your enemy." +
+				"\n    " + "As a bonus action, you have advantage on all Dexterity ability rolls and saving throws" +
+				"\n    " + "but disadvantage on all Strength ability rolls and saving throws while this is active." +
+				"\n" + "\u2022 The Berserker" + 
+				"\n    " + "All that matters is the blood being spilt." +
+				"\n    " + "It doesn't matter if its you or your opponent, someone must die!" +
+				"\n    " + "When this is activated as a bonus action, you have advantage on your attack rolls" +
+				"\n    " + "However, all attack rolls on you have advantage as well." +
+				"\n" + "\u2022 The Brute" +
+				"\n    " + "Use your strength to your advantage." +
+				"\n    " + "As a bonus action, you have advantage on any strength based ability roll or saving throw" +
+				"\n    " + "but have disadvantage on any dexterity based ability roll or saving throw." +
+				"\n" + "\u2022 The Dancer" +
+				"\n    " + "As a reaction, you can roll acrobatics to dodge an opponent's attack," +
+				"\n    " + "moving 5 feet to their left or right should you match or beat their attack roll." +
+				"\n" + "\u2022 The Defender" +
+				"\n    " + "As a reaction, you can ready your weapon to defend yourself," +
+				"\n    " + "adding your proficiency bonus to your AC when an enemy targets you with a weapon attack." +
+				"\n" + "\u2022 The Shield" +
+				"\n    " + "As a reaction, you can impose disadvantage on a " +
+				"\n    " + "creature that makes a melee attack on an ally within 5 feet of you." +
+				"\n    " + "However, all ranged attacks have advantage on you since you focused on melee defense.",
+				page3notes : true,
+				popupName : "Swordsman Stances",
+				source : ["S:TSMA", 0]
+			}]
+		},
+		"subclassfeature10" : {
+			name : "Weakpoints",
+			source : ["LoI"],
+			minlevel : 10,
+			description : "\n    " + "Before making an attack roll, you can take a -5 penalty to hit." +
+			"\n    " + "If you land an attack, that attack deals extra damage.",
+			additional : levels.map(function (n) {
+				return n < 10 ? "" : "+" + (n < 15 ? 2 : 3) + "d8";
+			})
+		},
+		"subclassfeature15" : {
+			name : "Flashstep",
+			source : ["LoI"],
+			minlevel : 15,
+			description : "\n    " + "Your movement speed for Blinkstep is equal to taking" +
+			"\n    " + "the dash action. In addition, you can move in any direction with" +
+			"\n    " + "Blinkstep, even if it isn't a straight line."
+		},
+		"subclassfeature18" : {
+			name : "Final Step",
+			source : ["LoI"],
+			minlevel : 18,
+			description : "\n    " + "As an action, initiate a Blinkstep against a creature." +
+			"\n    " + "Should you hit, you deal a massive amount of damage against the creature." +
+			"\n    " + "This can only be done once per short or long rest.",
+			additional : "10d8",
+			usages : 1,
+			action : [["action", ""]],
+			recovery : "long rest"
+		}
+	}
+});
 AddSubClass("fighter", "monster hunter-ua", {
 	regExpSearch : /^(?=.*monster)(?=.*hunter).*$/i,
 	subname : "Monster Hunter",
