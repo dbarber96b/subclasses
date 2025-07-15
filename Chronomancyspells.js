7766ae7e8363b2bb343ab1ca51ebb021a7a70a93
dbarber96b <87770160+dbarber96b@users.noreply.github.com>
2025-07-15 12:36:49 -0500

    Update Chronomancyspells.js

--- Chronomancyspells.js
+++ Chronomancyspells.js
@@ -8,7 +8,7 @@ SpellsList["fast-forward/reverse"] = {
 	range : "10 ft",
 	components : "V,S",
 	duration : "Conc, 1 h",
-	description : "Any number of non-magical objects sized small or smaller. Choose to fast-forward or reverese time in spall range.",
+	description : "Any number of non-magical objects sized small or smaller. Choose to fast-forward or reverese time in spell range.",
 	descriptionFull : "I manipulate the flow of time in a small area, rapidly altering the age of objects in the area as I choose. Select any number of non-magical objects of size Small or smaller in a 5 foot cube within range. Choose fast-forward/reverse as the area’s effect.." +
 		"\n   Fast-Forward: Ink dries; water boils, and candles burndown to stubs. You speed up time for the objects, causing them to experience 10 minutes for every minute spent casting the spell." +
 		"\n   Reverse: Rotting apples freshen, torn documents are mended, and shattered vases become whole. You reverse the aging process for the objects, causing them to appear as they did 10 minutes ago for every minute spent casting the spell."
@@ -121,4 +121,31 @@ SpellsList["alavar's spacetime anomaly"] = {
 	duration : "Conc, 1 min",
 	description : "I can designate any number of creatures I can see within 30-ft radius, they can use a bonus action to teleport to a space within the spell range.",
 	descriptionFull : "You create a disturbance in spacetime, causing the fabric of reality to collapse around you. You create a 30-foot radius area of warped space centered on you that remains stationary for the duration. When you cast this spell, you can designate any number of creatures you can see to be affected by it. For the duration, any affected creature within the spell's area can use a bonus action on each of their turns to slip through a dimensional pocket and teleport up to 30 feet to an unoccupied space that they can see that is also within the spell's area."
+};
+SpellsList["rewind"] = {
+	name : "Rewind",
+	classes : ["wizard"],
+	source : ["LoI"],
+	level : 7,
+	school : "Trans",
+	time : "1 a",
+	range : "500 feet",
+	components : "S",
+	duration : "Instantaneous",
+	description : "1 crea or obj in range returns to state they were before my last turn, spell slots and features are excluded.",
+	descriptionFull : "You fill a creature or object in range with arcane temporal energy that reverses their physical state while keeping them fixed in the present. The target returns to the exact state that it was in at the beginning of your last turn. If the target is a creature, they retain their memories. The target's hit points revert to the amount it had at the beginning of your last turn. Additionally, any status conditions or magical effects placed on it since that time are nullified, but any which were lifted since that time return. Spell slots and feature uses remain expended. For example, if a Paladin uses their Channel Divinity to use Abjure Enemy on a creature, and that creature is then immediately targeted by this spell, Abjure Enemy would no longer affect the creature, but the Paladin’s use of Channel Divinity would still be expended. The target also returns to the location that it was in at that time, if physically possible. Other nearby objects that interacted with the target maybe slightly moved or modified to account for this change. For example, if a creature is impaled by a spear and then targeted by this spell, the spear would be removed from their body as the wound is undone, settling on the ground next to them."
+};
+SpellsList["temporal bubble"] = {
+	name : "Temporal Bubble",
+	classes : ["Wizard"],
+	source : ["LoI"],
+	level : 8,
+	school : "Conj",
+	time : "1 hr",
+	range : "120 ft",
+	components : "V,S,M\u2020",
+	compMaterial : "An hourglass filled with pearl dust worth at least 2,500 gp, which the spell consumes",
+	duration : "8 h",
+	description : "Create 50-ft radius demiplane, inside 24 hours pass for 1 hour outside.",
+	descriptionFull : "You create a temporal shield around a creature other than you within range, keeping incoming wounds at bay by slowing them down. For the duration, each time the creature would take damage, the damage is instead shunted into the future. When the spell ends, the target takes all shunted damage at once. No part of this damage can be reduced by anything other than the resistances and immunities it had at the time that damage was shunted. The spell also ends early if the target is ever outside the spell’s range or if it has total cover from you."
 };
